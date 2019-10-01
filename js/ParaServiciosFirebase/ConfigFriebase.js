c2b7b68607d91d8580f14c8302e59c1e4df45e12
JuanJo53 <juanjofer1999@gmail.com>
2019-10-01 00:14:04 -0400

    archivos js para pruebas con firebase creados

---
+++ js/ParaServiciosFirebase/ConfigFriebase.js
@@ -0,0 +1,9 @@
+const varConfig = {
+    apiKey: "AIzaSyAEFS51wASyzXFPRgosvru8FHm-zvaMzAI",
+    authDomain: "printalo-ef2bc.firebaseapp.com",
+    databaseURL: "https://printalo-ef2bc.firebaseio.com",
+    projectId: "printalo-ef2bc",
+    storageBucket: "",
+    messagingSenderId: "917705384239",
+    appId: "1:917705384239:web:c7bb0ff68990454d84c1da"
+};

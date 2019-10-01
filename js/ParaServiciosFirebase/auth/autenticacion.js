63332e1b679f28f98ed7c64018fe6534a3580739
JuanJo53 <juanjofer1999@gmail.com>
2019-10-01 17:57:53 -0400

    solucion de conflictos en autenticacion

--- js/ParaServiciosFirebase/auth/autenticacion.js
+++ js/ParaServiciosFirebase/auth/autenticacion.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Autenticacion{
     autEmailPass (email,paswoord){
 
@@ -39,46 +38,4 @@ class Autenticacion{
                         
 
     }
-=======
-class Autenticacion{
-    autEmailPass (email,paswoord){
-
-    }
-
-    crearCuentaEmailPass (email,password, nombres){
-        firebase
-            .auth()
-            .createUserWithEmailAndPassword(email, password)
-            .then(result=>{
-                result.user.updateProfile({
-                    displayName : nombres
-                })
-
-                const configuracion = {
-                    url: '' 
-                }
-
-                result.user.sendEmail.Verification()
-                .catch(error=>{
-                    console.error(error)
-                    Materialize.toast(error.message, 4000)
-                })
-
-                firebase.auth().signOut()
-                Materialize.toast(
-                    'Bienbenido ${nombres}, debes realizar el proceso de verificacion',
-                    4000
-                )
-                $('.modal').modal('close')
-
-            })
-            .catch(error=>{
-                console.error(error)
-                Materialize.toast(error.message, 4000)
-            })
-
-                        
-
-    }
->>>>>>> 7169154353b0794f737e8fa3551194870f5636eb
 }

be17acb5a9d3848290d9095e0793017af05d5e66
JuanJo53 <juanjofer1999@gmail.com>
2019-10-01 17:56:02 -0400

    solucion de conflictos en configFriebase

--- js/ParaServiciosFirebase/ConfigFriebase.js
+++ js/ParaServiciosFirebase/ConfigFriebase.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const varConfig = {
     apiKey: "AIzaSyAEFS51wASyzXFPRgosvru8FHm-zvaMzAI",
     authDomain: "printalo-ef2bc.firebaseapp.com",
@@ -8,15 +7,3 @@ const varConfig = {
     messagingSenderId: "917705384239",
     appId: "1:917705384239:web:c7bb0ff68990454d84c1da"
 };
-=======
-//NO TOCAR ESTOS ARCHIVOS SE REQUIEREN MODIFICACIONES DEBIDO A CAMBIOS EN ARCHIVOS DE FRONTEND
-const varConfig = {
-    apiKey: "AIzaSyAEFS51wASyzXFPRgosvru8FHm-zvaMzAI",
-    authDomain: "printalo-ef2bc.firebaseapp.com",
-    databaseURL: "https://printalo-ef2bc.firebaseio.com",
-    projectId: "printalo-ef2bc",
-    storageBucket: "",
-    messagingSenderId: "917705384239",
-    appId: "1:917705384239:web:c7bb0ff68990454d84c1da"
-};
->>>>>>> 7169154353b0794f737e8fa3551194870f5636eb

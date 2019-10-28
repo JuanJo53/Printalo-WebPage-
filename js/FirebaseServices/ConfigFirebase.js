7f137f1bbf794dd6ded2258994fe182f879eb984
JuanJo53 <juanjofer1999@gmail.com>
2019-10-28 16:30:54 -0400

    Actualizacion de ConfigFirebase

--- js/FirebaseServices/ConfigFirebase.js
+++ js/FirebaseServices/ConfigFirebase.js
@@ -4,20 +4,9 @@ var firebaseConfig = {
     authDomain: "printalo-ef2bc.firebaseapp.com",
     databaseURL: "https://printalo-ef2bc.firebaseio.com",
     projectId: "printalo-ef2bc",
-    storageBucket: "",
+    storageBucket: "printalo-ef2bc.appspot.com",
     messagingSenderId: "917705384239",
     appId: "1:917705384239:web:c7bb0ff68990454d84c1da"
 };
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-/*
-function enviar(){
-    var email=document.getElementById('email').value;
-    var pass=document.getElementById('password').value;
-    alert("email "+email+" pass "+pass);
-    firebase.auth().createUserWithEmailAndPassword(email,pass)
-    .catch(function(error){
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        alert(errorMessage);
-    });
-}*/

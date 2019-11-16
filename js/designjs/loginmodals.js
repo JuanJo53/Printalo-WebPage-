0ca4a4551a5803ebae6bbf225ccd21a85c874083
ariel <ariel.colque.herrera@gmail.com>
2019-11-16 09:34:27 -0400

    Se agrego registro de negocio en INDEXNEG

--- js/designjs/loginmodals.js
+++ js/designjs/loginmodals.js
@@ -2,3 +2,8 @@ $('.modal-neg').on('click',function(e){
     e.preventDefault();
     $('#modalNegocio').modal('show').find('.modal-dialog').load($(this).attr('href'));
 });
+
+$('.modal-register-neg').on('click',function(e){
+    e.preventDefault();
+    $('#modalRegisterNegocio').modal('show').find('.modal-dialog').load($(this).attr('href'));
+});

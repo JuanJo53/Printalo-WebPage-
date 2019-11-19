7d7eddbf29ee58da7c518594911420558bd49b83
JuanJo53 <juanjofer1999@gmail.com>
2019-11-19 18:29:22 -0400

    botnoes editar precios con interactividad

---
+++ js/Negocio/confPrecios.js
@@ -0,0 +1,14 @@
+function enableBN(){
+    if(document.getElementById("txtPrecioBN").disabled === false){
+        document.getElementById("txtPrecioBN").disabled = true;
+    }else{
+        document.getElementById("txtPrecioBN").disabled = false;
+    }    
+}
+function enableColor(){
+    if(document.getElementById("txtPrecioColor").disabled === false){
+        document.getElementById("txtPrecioColor").disabled = true;
+    }else{
+        document.getElementById("txtPrecioColor").disabled = false;
+    }    
+}

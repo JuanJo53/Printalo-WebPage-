99193a0c5e2d529c190d5eea92c12b1ff8886894
ariel <ariel.colque.herrera@gmail.com>
2019-11-19 00:56:15 -0400

    Se divido el avatar para empleado y para administrador

--- js/designjs/perfiliconuser.js
+++ js/designjs/perfiliconuser.js
@@ -1,4 +1,18 @@
+//llama a perfil de usuario
 $('.icon-user').on('click',function(e){
     e.preventDefault();
     $('#perfiliconusuario').modal('show').find('.modal-content').load($(this).attr('href'));
 });
+//--llama a perfil de usuario
+//llama a perfil de administrador
+$('.icon-user').on('click',function(e){
+    e.preventDefault();
+    $('#perfiliconadmi').modal('show').find('.modal-content').load($(this).attr('href'));
+});
+//--llama a perfil de administrador
+//llama a perfil de empleado
+$('.icon-user').on('click',function(e){
+    e.preventDefault();
+    $('#perfiliconemp').modal('show').find('.modal-content').load($(this).attr('href'));
+});
+//--llama a perfil de empleado

8b93ab382e3125b15c11e6c1681005337214b2cd
JuanJo53 <juanjofer1999@gmail.com>
2019-11-19 01:52:15 -0400

    inicial de avatars de clientes logrado al 100%

--- js/Cliente/porEnviar.js
+++ js/Cliente/porEnviar.js
@@ -1,43 +1,36 @@
+window.onload=ValidarCli();
 //Set inicial en avatar de web
-var user = firebase.auth().currentUser;
-var bd=firebase.firestore();
-var userid= user.uid;
-var nombre;
-console.log(userid);
-var docRef = bd.collection('Clientes').doc(userid);
-docRef.get().then(function(doc){
-    if (doc.exists){
-        console.log("Datos de Clientes:", doc.data());
-        nombre=doc.data().Nombre;
-        var inicial=nombre.charAt(0);
-        console.log(inicial);
-        document.getElementById('IncialCliente').innerHTML  = inicial;
-        document.getElementById('mdlCliente').innerHTML  = inicial;
-    }else{
-        console.log("No such document!");
-    }
-}).catch(function(error){
-    console.log("Error al obtener los datos:", error);
-})
-
-// Evento de Boton Cerrar Secion
-function Salir(){
-    // Esta funcion pasa el email y su password a la clase Auth para logout con firebase
-    var auth = new Auth();
-    auth.Logout();
-    ValidarCli();
-};
-
+function SetInicial(){
+    var user = firebase.auth().currentUser;
+    var bd=firebase.firestore();
+    var userid= user.uid;
+    var nombre;
+    console.log(userid);
+    var docRef = bd.collection('Clientes').doc(userid);
+    docRef.get().then(function(doc){
+        if (doc.exists){
+            console.log("Datos de Clientes:", doc.data());
+            nombre=doc.data().Nombre;
+            var inicial=nombre.charAt(0);
+            console.log(inicial);
+            document.getElementById('IncialCliente').innerHTML  = inicial;
+        }else{
+            console.log("No such document!");
+        }
+    }).catch(function(error){
+        console.log("Error al obtener los datos:", error);
+    })   
+}
 // Esta funcion ejecuta el observador de firebase
 function ValidarCli(){
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             // User is signed in.
-            alert("Logeado"); 
-            location.href="/html/usuarioUI/documentosCli/porEnviar.html"
+            SetInicial();
+            console.log("Logeado");
         }else{
             // User is not signed in.
-            alert("No Logeado");
+            console.log("No Logeado");
             location.href="/html/index/usuarioIndex/indexUser.html"
         }
     });

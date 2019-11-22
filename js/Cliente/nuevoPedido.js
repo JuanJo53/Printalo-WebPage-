6c1c951d38406a1bf702c7786749f6bcbceb25be
JuanJo53 <juanjofer1999@gmail.com>
2019-11-22 00:31:44 -0400

    subida de documento a storage y base de datos logrado

--- js/Cliente/nuevoPedido.js
+++ js/Cliente/nuevoPedido.js
@@ -1,24 +1,47 @@
 var fileButton = document.getElementById('my-file');
 fileButton.addEventListener('change',function (e){
     var storage = firebase.storage();
+    var user = firebase.auth().currentUser;
+    var bd = firebase.firestore();
     //Get file
     var file=e.target.files[0];
-    console.log('hola');
+    console.log(file);
     //Create a storage ref
-    var storageRef = storage.ref('documentos/'+file);
-
+    var storageRef = storage.ref('docsPedidos/'+user.uid+'/'+file.name);
     //Upload file
     storageRef.put(file);
-
-    //Update progress bar
-
+    var task=storageRef.put(file);
+    
+    task.on('state_changed', null, null, function() {
+        task.snapshot.ref.getDownloadURL()
+        .then(url=>{
+            console.log("Mostrar: "+url);
+            bd.collection('Pedido').add({
+                blancoYnegro: false,
+                cantidad: 0,
+                clienteID: user.uid,
+                docID: url,
+                engrampado: false,
+                estado: "porEnviar",
+                fecha: firebase.firestore.FieldValue.serverTimestamp(),
+                fechaEntrega: firebase.firestore.FieldValue.serverTimestamp(),
+                ladosImpre: "",
+                metodoPago: "",
+                negocioID: "",
+                paginas: true,
+                tamañoHoja: "",
+                tipoHoja: ""
+            })
+        })
+        
+      });
+    
 });
 // Esta funcion ejecuta el observador de firebase
 function ValidarCli(){
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             // User is signed in.
-            getDoc();
             console.log("Logeado");
         }else{
             // User is not signed in.

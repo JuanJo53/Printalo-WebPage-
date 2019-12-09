45cea689eae45eee7fe1cfabff5dbf7adb6fe1d2
JuanJo53 <juanjofer1999@gmail.com>
2019-12-09 10:06:48 -0400

    visualizacion de detalles para antes de aceptar pedido (negocio)

--- js/Pedidos/Pedido.js
+++ js/Pedidos/Pedido.js
@@ -62,28 +62,117 @@ class Pedido {
 				});
 			})
 			.catch(function(error) {
-				console.log("Documento no se borro correctamente");
+				console.log("Documento no se borro co(rrectamente");
 			});
 	}
+	//Rechazar pedido entrante.
 	rechazarPedido(doc, user, precio, cant, pago, fecha, hora) {
 		var user = firebase.auth().currentUser;
 		var bd = firebase.firestore();
-		var storage = firebase.storage();
-		var storageRef = storage.ref();
 		console.log(doc, user, precio, cant, pago, fecha, hora);
-		var user = firebase.auth().currentUser;
-
+		var f=fecha.split('/');
+		var d=f[0];
+		var m=f[1];
+		var a=f[2];
+		var timestamp=new Date(a+'-'+m+'-'+d+' '+hora);
 		var query = bd
 			.collection("Pedido")
 			.where("nombreDoc", "==", doc)
-			.where("negocioID", "==", user.uid);
+			.where("negocioID", "==", user.uid)
+			.where('fechaEntrega','==',timestamp);
 		query
 			.get()
 			.then(function(querySnapshot) {
-				querySnapshot.forEach(function(doc) {});
+				querySnapshot.forEach(function(doc) {
+					bd.collection('Pedido').doc(doc.id).update({
+						estado: "porEnviar"
+					}).then(function(){
+						location.reload();
+						console.log("Documento se rechazo correctamente");		
+					});
+				});
 			})
 			.catch(function(error) {
 				console.log("Documento no se rechazo correctamente");
 			});
 	}
+	//Setea los detalles del pedido para visualizacion antes de aceptarlo.
+	setPedDet(doc, usuario, precio, cant, pago, fechaE, horaE){
+		var user = firebase.auth().currentUser;
+		var bd = firebase.firestore();
+		var 
+		color,
+		tam,
+		imp,
+		paginas,
+		acabado,
+		tipo,
+		fecha,
+		hora;
+		var f=fechaE.split('/');
+		var d=f[0];
+		var m=f[1];
+		var a=f[2];
+		var timestamp=new Date(a+'-'+m+'-'+d+' '+horaE);
+		var query = bd
+			.collection("Pedido")
+			.where("nombreDoc", "==", doc)
+			.where("negocioID", "==", user.uid)
+			.where('fechaEntrega','==',timestamp);
+		query
+			.get()
+			.then(function(querySnapshot) {
+				querySnapshot.forEach(function(doc) {
+					if (doc.data().blancoYnegro === false) {
+						document.getElementById("colorD").value = 'a Color';
+					} else {
+						document.getElementById("colorD").value = 'Blanco y Negro';
+					}
+					if (doc.data().tamañoHoja === "carta") {
+						document.getElementById("tamanioD").value = 'carta';
+					} else if (doc.data().tamañoHoja === "oficio") {
+						document.getElementById("tamanioD").value = 'oficio';
+					} else {
+						document.getElementById("tamanioD").value = 'A4';
+					}
+					if (doc.data().ladosImpre === "intercalado") {
+						document.getElementById("impresionD").value ='intercalado';
+					} else {
+						document.getElementById("impresionD").value ='anv/rev';
+					}
+					document.getElementById("numPagD").value = doc.data().numPaginas;
+					if (doc.data().engrampado === true) {
+						document.getElementById("acabadoD").value = 'Si';
+					} else {
+						document.getElementById("acabadoD").value = 'No';
+					}
+					if (doc.data().tipo === "normal") {
+						document.getElementById("tipoHojaD").value = 'Hoja Normal';
+					} else {
+						document.getElementById("tipoHojaD").value = 'Hoja Normal';
+					}
+					document.getElementById("cantD").value = doc.data().cantidad;
+					if (doc.data().metodoPago === "personal") {
+						document.getElementById("pagoD").value = 'Personal';
+					} else {
+						document.getElementById("pagoD").value = 'Tarjeta de Credito';
+					}
+					document.getElementById("clienteD").value = usuario;
+					timestamp = new Date(doc.data().fecha.toDate());
+					fecha =
+						timestamp.getDate() + "/" + (timestamp.getMonth() + 1) + "/" + timestamp.getFullYear();
+					hora = timestamp.getHours() + ":" + timestamp.getMinutes();
+					document.getElementById("fechaD").value = fecha;
+					document.getElementById("horaD").value = hora;
+					document.getElementById("fEntregaD").value = fechaE;
+					document.getElementById("hEntregaD").value = horaE;
+					document.getElementById("costoD").value = doc.data().costoTotal;
+					document.getElementById("estadoD").value = doc.data().estado;
+				});
+			})
+			.catch(function(error) {
+				console.log("Documento no se rechazo correctamente");
+			});
+	}
+	
 }

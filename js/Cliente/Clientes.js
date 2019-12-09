f85d03f44e0343e5c20ed849cd76bdc0d81e4f0f
JuanJo53 <juanjofer1999@gmail.com>
2019-12-08 23:53:45 -0400

    correcciones al subir a la base de datos (config clientes)

--- js/Cliente/Clientes.js
+++ js/Cliente/Clientes.js
@@ -1,157 +1,152 @@
 class Cliente {
-  constructor(email, password, nombre, apellido, telefono) {
-    this.email = email;
-    this.password = password;
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.telefono = telefono;
+	constructor(email, password, nombre, apellido, telefono) {
+		this.email = email;
+		this.password = password;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.telefono = telefono;
 
-    //Instancia para valores de firestore
-    this.bd = firebase.firestore();
-  }
-  // Esta funcion pasa el email y su password a la clase Auth para registrar un nuevo usuario en firebase
-  RegistrarCli(a, e, n, t) {
-    var auth = new Auth();
-    auth.crearCuentaEmailPass(this.email, this.password);
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        // User is signed in.
-        var user = firebase.auth().currentUser;
-        console.log(user);
-        firebase
-          .firestore()
-          .collection("Clientes")
-          .doc(user.uid)
-          .set({
-            Apellido: a,
-            email: e,
-            Nombre: n,
-            telefono: t
-          })
-          .then(e => {
-            alert("Logeado");
-            location.href = "/html/usuarioUI/documentosCli/porEnviar.html";
-          })
-          .catch(e => {
-            console.log(`Error creando cliente: ${error}`);
-          });
-      } else {
-        // User is not signed in.
-        alert("No Logeado");
-      }
-    });
-  }
-  // Esta funcion pasa el email y su password a la clase Auth para login con firebase
-  IngresarCli() {
-    var auth = new Auth();
-    auth.LoginEmailPass(this.email, this.password);
-  }
-  //funcion de cerrar sesion aqui!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		//Instancia para valores de firestore
+		this.bd = firebase.firestore();
+	}
+	// Esta funcion pasa el email y su password a la clase Auth para registrar un nuevo usuario en firebase
+	RegistrarCli(a, e, n, t) {
+		var auth = new Auth();
+		auth.crearCuentaEmailPass(this.email, this.password);
+		firebase.auth().onAuthStateChanged(function(user) {
+			if (user) {
+				// User is signed in.
+				var user = firebase.auth().currentUser;
+				console.log(user);
+				firebase
+					.firestore()
+					.collection("Clientes")
+					.doc(user.uid)
+					.set({
+						Apellido: a,
+						email: e,
+						Nombre: n,
+						telefono: t
+					})
+					.then(e => {
+						alert("Logeado");
+						location.href = "/html/usuarioUI/documentosCli/porEnviar.html";
+					})
+					.catch(e => {
+						console.log(`Error creando cliente: ${error}`);
+					});
+			} else {
+				// User is not signed in.
+				alert("No Logeado");
+			}
+		});
+	}
+	// Esta funcion pasa el email y su password a la clase Auth para login con firebase
+	IngresarCli() {
+		var auth = new Auth();
+		auth.LoginEmailPass(this.email, this.password);
+	}
+	//funcion de cerrar sesion aqui!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-  //Setea los datos actuales del usuario
-  setDatosPerfilUsuarioAct() {
-    var nombreUsuario, apellidoUsuario, fonoUsuario, emailUsuario;
-    var user = firebase.auth().currentUser;
-    var bd = firebase.firestore();
-    var userid = user.uid;
-    var docRef = bd.collection("Clientes").doc(userid);
-    docRef
-      .get()
-      .then(function(doc) {
-        if (doc.exists) {
-          nombreUsuario = doc.data().Nombre;
-          apellidoUsuario = doc.data().Apellido;
-          fonoUsuario = doc.data().telefono;
-          emailUsuario = doc.data().email;
-          document.getElementById("nombreUsuario").value = nombreUsuario;
-          document.getElementById("apellidoUsuario").value = apellidoUsuario;
-          document.getElementById("telefonoUsuario").value = fonoUsuario;
-          document.getElementById("emailUsuario").value = emailUsuario;
-        } else {
-          console.log("No existe el documento!");
-        }
-      })
-      .catch(function(error) {
-        console.log("Error al obtener los datos:", error);
-      });
-  }
+	//Setea los datos actuales del usuario
+	setDatosPerfilUsuarioAct() {
+		var nombreUsuario, apellidoUsuario, fonoUsuario, emailUsuario;
+		var user = firebase.auth().currentUser;
+		var bd = firebase.firestore();
+		var userid = user.uid;
+		var docRef = bd.collection("Clientes").doc(userid);
+		docRef
+			.get()
+			.then(function(doc) {
+				if (doc.exists) {
+					nombreUsuario = doc.data().Nombre;
+					apellidoUsuario = doc.data().Apellido;
+					fonoUsuario = doc.data().telefono;
+					emailUsuario = doc.data().email;
+					document.getElementById("nombreUsuario").value = nombreUsuario;
+					document.getElementById("apellidoUsuario").value = apellidoUsuario;
+					document.getElementById("telefonoUsuario").value = fonoUsuario;
+					document.getElementById("emailUsuario").value = emailUsuario;
+				} else {
+					console.log("No existe el documento!");
+				}
+			})
+			.catch(function(error) {
+				console.log("Error al obtener los datos:", error);
+			});
+	}
 
-  //guarda datos generales de admnistrador
-  GuardarCambiosPerfilUsuario() {
-    //console.log("click");
-    var apelUsu, nomUsu, telUsu, corUsu;
-    var nombreUsuario, apellidoUsuario, telefonoUsuario, correoUsuario;
-    nomUsu = document.getElementById("nombreUsuario").value;
-    apelUsu = document.getElementById("apellidoUsuario").value;
-    telUsu = document.getElementById("telefonoUsuario").value;
-    corUsu = document.getElementById("emailUsuario").value;
-    var user = firebase.auth().currentUser;
-    var bd = firebase.firestore();
-    var userid = user.uid;
-    var docRef = bd.collection("Clientes").doc(userid);
-    docRef
-      .get()
-      .then(function(doc) {
-        if (doc.exists) {
-          nombreUsuario = doc.data().Nombre;
-          apellidoUsuario = doc.data().Apellido;
-          telefonoUsuario = doc.data().telefono;
-          correoUsuario = doc.data().email;
-          if (
-            nombreUsuario === nomUsu &&
-            apellidoUsuario === apelUsu &&
-            telefonoUsuario === telUsu &&
-            correoUsuario === corUsu
-          ) {
-            console.log("los datos son los mismos");
-          } else {
-            //console.log("son distintos");
-            actualizarDatosPerfilUsuario();
-            alert("Se guardo cambios");
-          }
-        } else {
-          console.log("No existe el documento!");
-        }
-      })
-      .catch(function(error) {
-        console.log("Error al obtener los datos:", error);
-      });
-  }
+	//guarda datos generales de admnistrador
+	GuardarCambiosPerfilUsuario() {
+		//console.log("click");
+		var apelUsu, nomUsu, telUsu, corUsu;
+		var nombreUsuario, apellidoUsuario, telefonoUsuario, correoUsuario;
+		nomUsu = document.getElementById("nombreUsuario").value;
+		apelUsu = document.getElementById("apellidoUsuario").value;
+		telUsu = document.getElementById("telefonoUsuario").value;
+		corUsu = document.getElementById("emailUsuario").value;
+		var user = firebase.auth().currentUser;
+		var bd = firebase.firestore();
+		var userid = user.uid;
+		var docRef = bd.collection("Clientes").doc(userid);
+		docRef
+			.get()
+			.then(function(doc) {
+				if (doc.exists) {
+					nombreUsuario = doc.data().Nombre;
+					apellidoUsuario = doc.data().Apellido;
+					telefonoUsuario = doc.data().telefono;
+					correoUsuario = doc.data().email;
+					if (
+						nombreUsuario === nomUsu &&
+						apellidoUsuario === apelUsu &&
+						telefonoUsuario === telUsu &&
+						correoUsuario === corUsu
+					) {
+						console.log("los datos son los mismos");
+					} else {
+						//console.log("son distintos");
+						actualizarDatosPerfilUsuario();
+						alert("Se guardo cambios");
+					}
+				} else {
+					console.log("No existe el documento!");
+				}
+			})
+			.catch(function(error) {
+				console.log("Error al obtener los datos:", error);
+			});
+	}
 }
 //esta funcion actualiza los datos de datos generales
 function actualizarDatosPerfilUsuario() {
-  //console.log("entro a cambiar datos de negocio");
-  var nombreUsuario, apellidoUsuario, fonoUsuario, emailUsuario;
-  var user = firebase.auth().currentUser;
-  var bd = firebase.firestore();
-  var userid = user.uid;
-  nombreUsuario = document.getElementById("nombreUsuario").value;
-  apellidoUsuario = document.getElementById("apellidoUsuario").value;
-  fonoUsuario = document.getElementById("telefonoUsuario").value;
-  emailUsuario = document.getElementById("emailUsuario").value;
-  //verifica que no este vacios los campos
-  if (
-    nombreUsuario !== "" &&
-    apellidoUsuario !== "" &&
-    fonoUsuario !== "" &&
-    emailUsuario !== ""
-  ) {
-    //console.log("no estan vacios");
-    bd.collection("Clientes")
-      .doc(user.uid)
-      .update({
-        Nombre: nombreUsuario,
-        Apellido: apellidoUsuario,
-        email: fonoUsuario,
-        telefono: emailUsuario
-      })
-      .then(e => {
-        console.log("Datos Guardados Exitosamente datos generales");
-      })
-      .catch(e => {
-        alert(`Error Guardando Datos: ${error}`);
-      });
-  } else {
-    console.log("los campos estan vacios");
-  }
+	//console.log("entro a cambiar datos de negocio");
+	var nombreUsuario, apellidoUsuario, fonoUsuario, emailUsuario;
+	var user = firebase.auth().currentUser;
+	var bd = firebase.firestore();
+	var userid = user.uid;
+	nombreUsuario = document.getElementById("nombreUsuario").value;
+	apellidoUsuario = document.getElementById("apellidoUsuario").value;
+	fonoUsuario = document.getElementById("telefonoUsuario").value;
+	emailUsuario = document.getElementById("emailUsuario").value;
+	//verifica que no este vacios los campos
+	if (nombreUsuario !== "" && apellidoUsuario !== "" && fonoUsuario !== "" && emailUsuario !== "") {
+		//console.log("no estan vacios");
+		bd.collection("Clientes")
+			.doc(user.uid)
+			.update({
+				Nombre: nombreUsuario,
+				Apellido: apellidoUsuario,
+				email: emailUsuario,
+				telefono: fonoUsuario
+			})
+			.then(e => {
+				console.log("Datos Guardados Exitosamente datos generales");
+			})
+			.catch(e => {
+				alert(`Error Guardando Datos: ${error}`);
+			});
+	} else {
+		console.log("los campos estan vacios");
+	}
 }

3dc1a3d13d62d00cfb7efe43f23f5a50df08ba46
JuanJo53 <juanjofer1999@gmail.com>
2019-12-16 19:51:53 -0400

    preparacion de factura realizado, falta hacer la entrega como tal

--- js/Pedidos/pedPorPagar.js
+++ js/Pedidos/pedPorPagar.js
@@ -92,7 +92,7 @@ function setData(doc, nomb, prec, cant, pag, f, h) {
         class="far fa-check-square fa-2x"></i></div>`,
 				`<button id="detalles" href="" onclick="getPedDet(this)" class="btn bg-printalo-greenDetail positive" data-dismiss="modal"
         data-target="#modalVerDetalles" data-toggle="modal">Detalles</button>`,
-				`<button href="" class="btn bg-printalo-greenDetail positive" data-dismiss="modal"
+				`<button id="entregar" onclick="getPedDet(this)" href="" class="btn bg-printalo-greenDetail positive" data-dismiss="modal"
         data-target="#entregarPedido" data-toggle="modal">Entregar</button>`
 			])
 			.draw(false);
@@ -169,7 +169,11 @@ function getPedDet(_this) {
 	pago = getRowSelected(_this, 4);
 	fechaE = getRowSelected(_this, 5);
 	horaE = getRowSelected(_this, 6);
-	pedido.setPedDet(doc, usuario, precio, cant, pago, fechaE, horaE);
+	if (_this.id === "detalles") {
+		pedido.setPedDet(doc, usuario, precio, cant, pago, fechaE, horaE);
+	} else {
+		presetFac(doc, precio, cant, pago, fechaE, horaE);
+	}
 }
 //Obtiene el nombre de la fila seleccionada.
 function getRowSelected(objectPressed, col) {
@@ -177,10 +181,193 @@ function getRowSelected(objectPressed, col) {
 	var nomb = a.getElementsByTagName("td")[col].innerHTML;
 	return nomb;
 }
-function entregarPedido() {
-	var nombre = document.getElementById("facNombCli").value;
-	var nit = document.getElementById("facNitCli").value;
+function presetFac(doc, precio, cant, pago, fechaE, horaE) {
+	var imprFac = document.getElementById("imprimirFac");
+	imprFac.addEventListener("click", f => {
+		var apellido = document.getElementById("apllCli").value;
+		var nit = document.getElementById("nitCli").value;
+		var precioLit = NumeroALetras(precio);
+		var pedido = new Pedido();
+		pedido.setDatFactura(nit, apellido, doc, precio, precioLit, cant, pago, fechaE, horaE);
+	});
 }
+function Unidades(num) {
+	switch (num) {
+		case 1:
+			return "UN";
+		case 2:
+			return "DOS";
+		case 3:
+			return "TRES";
+		case 4:
+			return "CUATRO";
+		case 5:
+			return "CINCO";
+		case 6:
+			return "SEIS";
+		case 7:
+			return "SIETE";
+		case 8:
+			return "OCHO";
+		case 9:
+			return "NUEVE";
+	}
+
+	return "";
+} //Unidades()
+
+function Decenas(num) {
+	decena = Math.floor(num / 10);
+	unidad = num - decena * 10;
+
+	switch (decena) {
+		case 1:
+			switch (unidad) {
+				case 0:
+					return "DIEZ";
+				case 1:
+					return "ONCE";
+				case 2:
+					return "DOCE";
+				case 3:
+					return "TRECE";
+				case 4:
+					return "CATORCE";
+				case 5:
+					return "QUINCE";
+				default:
+					return "DIECI" + Unidades(unidad);
+			}
+		case 2:
+			switch (unidad) {
+				case 0:
+					return "VEINTE";
+				default:
+					return "VEINTI" + Unidades(unidad);
+			}
+		case 3:
+			return DecenasY("TREINTA", unidad);
+		case 4:
+			return DecenasY("CUARENTA", unidad);
+		case 5:
+			return DecenasY("CINCUENTA", unidad);
+		case 6:
+			return DecenasY("SESENTA", unidad);
+		case 7:
+			return DecenasY("SETENTA", unidad);
+		case 8:
+			return DecenasY("OCHENTA", unidad);
+		case 9:
+			return DecenasY("NOVENTA", unidad);
+		case 0:
+			return Unidades(unidad);
+	}
+} //Unidades()
+
+function DecenasY(strSin, numUnidades) {
+	if (numUnidades > 0) return strSin + "Y" + Unidades(numUnidades);
+
+	return strSin;
+} //DecenasY()
+
+function Centenas(num) {
+	centenas = Math.floor(num / 100);
+	decenas = num - centenas * 100;
+
+	switch (centenas) {
+		case 1:
+			if (decenas > 0) return "CIEN" + Decenas(decenas);
+			return "CIEN";
+		case 2:
+			return "DOSCIENTOS" + Decenas(decenas);
+		case 3:
+			return "TRESCIENTOS" + Decenas(decenas);
+		case 4:
+			return "CUATROCIENTOS" + Decenas(decenas);
+		case 5:
+			return "QUINIENTOS" + Decenas(decenas);
+		case 6:
+			return "SEISCIENTOS" + Decenas(decenas);
+		case 7:
+			return "SETECIENTOS" + Decenas(decenas);
+		case 8:
+			return "OCHOCIENTOS" + Decenas(decenas);
+		case 9:
+			return "NOVECIENTOS" + Decenas(decenas);
+	}
+
+	return Decenas(decenas);
+} //Centenas()
+
+function Seccion(num, divisor, strSingular, strPlural) {
+	cientos = Math.floor(num / divisor);
+	resto = num - cientos * divisor;
+
+	letras = "";
+
+	if (cientos > 0)
+		if (cientos > 1) letras = Centenas(cientos) + " " + strPlural;
+		else letras = strSingular;
+
+	if (resto > 0) letras += "";
+
+	return letras;
+} //Seccion()
+
+function Miles(num) {
+	divisor = 1000;
+	cientos = Math.floor(num / divisor);
+	resto = num - cientos * divisor;
+
+	strMiles = Seccion(num, divisor, "UN MIL", "MIL");
+	strCentenas = Centenas(resto);
+
+	if (strMiles == "") return strCentenas;
+
+	return strMiles + " " + strCentenas;
+} //Miles()
+
+function Millones(num) {
+	divisor = 1000000;
+	cientos = Math.floor(num / divisor);
+	resto = num - cientos * divisor;
+
+	strMillones = Seccion(num, divisor, "UN MILLON DE", "MILLONES DE");
+	strMiles = Miles(resto);
+
+	if (strMillones == "") return strMiles;
+
+	return strMillones + " " + strMiles;
+} //Millones()
+
+function NumeroALetras(num) {
+	var data = {
+		numero: num,
+		enteros: Math.floor(num),
+		centavos: Math.round(num * 100) - Math.floor(num) * 100,
+		letrasCentavos: "",
+		letrasMonedaPlural: "BOLIVIANO", //“PESOS”, 'Dólares', 'Bolívares', 'etcs'
+		letrasMonedaSingular: "BOLIVIANO", //“PESO”, 'Dólar', 'Bolivar', 'etc'
+
+		letrasMonedaCentavoPlural: "CENTAVOS",
+		letrasMonedaCentavoSingular: "CENTAVO"
+	};
+
+	if (data.centavos > 0) {
+		data.letrasCentavos =
+			"CON " +
+			(function() {
+				if (data.centavos == 1)
+					return Millones(data.centavos) + " " + data.letrasMonedaCentavoSingular;
+				else return Millones(data.centavos) + " " + data.letrasMonedaCentavoPlural;
+			})();
+	}
+
+	if (data.enteros == 0) return "CERO " + data.letrasMonedaPlural + " " + data.letrasCentavos;
+	if (data.enteros == 1)
+		return Millones(data.enteros) + " " + data.letrasMonedaSingular + " " + data.letrasCentavos;
+	else return Millones(data.enteros) + " " + data.letrasMonedaPlural + " " + data.letrasCentavos;
+} //NumeroALetras()
 //Esta funcion ejecuta el observador de firebase
 function ValidarCli() {
 	firebase.auth().onAuthStateChanged(function(user) {

2a1c84414c33f1c739fea9e18f1f797d9a9d9f75
JuanJo53 <juanjofer1999@gmail.com>
2019-12-15 22:23:19 -0400

    datos de tarjeta confirados para edicion

--- js/Cliente/Clientes.js
+++ js/Cliente/Clientes.js
@@ -26,7 +26,12 @@ class Cliente {
 						Apellido: a,
 						email: e,
 						Nombre: n,
-						telefono: t
+						telefono: t,
+						nombTarjeta: "",
+						numTarjeta: "",
+						mesTarjeta: 0,
+						anioTarjeta: 0,
+						cvvTarjeta: 0
 					})
 					.then(e => {
 						alert("Logeado");
@@ -50,7 +55,15 @@ class Cliente {
 
 	//Setea los datos actuales del usuario
 	setDatosPerfilUsuarioAct() {
-		var nombreUsuario, apellidoUsuario, fonoUsuario, emailUsuario;
+		var nombreUsuario,
+			apellidoUsuario,
+			fonoUsuario,
+			emailUsuario,
+			nombTarjeta,
+			numTarjeta,
+			mesTarjeta,
+			anioTarjeta,
+			cvvTarjeta;
 		var user = firebase.auth().currentUser;
 		var bd = firebase.firestore();
 		var userid = user.uid;
@@ -63,10 +76,20 @@ class Cliente {
 					apellidoUsuario = doc.data().Apellido;
 					fonoUsuario = doc.data().telefono;
 					emailUsuario = doc.data().email;
+					nombTarjeta = doc.data().nombTarjeta;
+					numTarjeta = doc.data().numTarjeta;
+					mesTarjeta = doc.data().mesTarjeta;
+					anioTarjeta = doc.data().anioTarjeta;
+					cvvTarjeta = doc.data().cvvTarjeta;
 					document.getElementById("nombreUsuario").value = nombreUsuario;
 					document.getElementById("apellidoUsuario").value = apellidoUsuario;
 					document.getElementById("telefonoUsuario").value = fonoUsuario;
 					document.getElementById("emailUsuario").value = emailUsuario;
+					document.getElementById("Tnombre").value = nombTarjeta;
+					document.getElementById("Tnumero").value = numTarjeta;
+					document.getElementById("Tmes").value = mesTarjeta;
+					document.getElementById("Tanio").value = anioTarjeta;
+					document.getElementById("Tcvv").value = cvvTarjeta;
 				} else {
 					console.log("No existe el documento!");
 				}
@@ -79,12 +102,25 @@ class Cliente {
 	//guarda datos generales de admnistrador
 	GuardarCambiosPerfilUsuario() {
 		//console.log("click");
-		var apelUsu, nomUsu, telUsu, corUsu;
-		var nombreUsuario, apellidoUsuario, telefonoUsuario, correoUsuario;
+		var apelUsu, nomUsu, telUsu, corUsu, nombTar, numTar, mesTar, anioTar, cvvTar;
+		var nombreUsuario,
+			apellidoUsuario,
+			telefonoUsuario,
+			correoUsuario,
+			nombTarjeta,
+			numTarjeta,
+			mesTarjeta,
+			anioTarjeta,
+			cvvTarjeta;
 		nomUsu = document.getElementById("nombreUsuario").value;
 		apelUsu = document.getElementById("apellidoUsuario").value;
 		telUsu = document.getElementById("telefonoUsuario").value;
 		corUsu = document.getElementById("emailUsuario").value;
+		nombTar = document.getElementById("Tnombre").value;
+		numTar = document.getElementById("Tnumero").value;
+		mesTar = document.getElementById("Tmes").value;
+		anioTar = document.getElementById("Tanio").value;
+		cvvTar = document.getElementById("Tcvv").value;
 		var user = firebase.auth().currentUser;
 		var bd = firebase.firestore();
 		var userid = user.uid;
@@ -97,11 +133,21 @@ class Cliente {
 					apellidoUsuario = doc.data().Apellido;
 					telefonoUsuario = doc.data().telefono;
 					correoUsuario = doc.data().email;
+					nombTarjeta = doc.data().Nombre;
+					numTarjeta = doc.data().Apellido;
+					mesTarjeta = doc.data().telefono;
+					anioTarjeta = doc.data().email;
+					cvvTarjeta = doc.data().email;
 					if (
 						nombreUsuario === nomUsu &&
 						apellidoUsuario === apelUsu &&
 						telefonoUsuario === telUsu &&
-						correoUsuario === corUsu
+						correoUsuario === corUsu &&
+						nombTarjeta === nombTar &&
+						numTarjeta === numTar &&
+						mesTarjeta === mesTar &&
+						anioTarjeta === anioTar &&
+						cvvTarjeta === cvvTar
 					) {
 						console.log("los datos son los mismos");
 					} else {
@@ -121,7 +167,15 @@ class Cliente {
 //esta funcion actualiza los datos de datos generales
 function actualizarDatosPerfilUsuario() {
 	//console.log("entro a cambiar datos de negocio");
-	var nombreUsuario, apellidoUsuario, fonoUsuario, emailUsuario;
+	var nombreUsuario,
+		apellidoUsuario,
+		fonoUsuario,
+		emailUsuario,
+		nombTarjeta,
+		numTarjeta,
+		mesTarjeta,
+		anioTarjeta,
+		cvvTarjeta;
 	var user = firebase.auth().currentUser;
 	var bd = firebase.firestore();
 	var userid = user.uid;
@@ -129,6 +183,11 @@ function actualizarDatosPerfilUsuario() {
 	apellidoUsuario = document.getElementById("apellidoUsuario").value;
 	fonoUsuario = document.getElementById("telefonoUsuario").value;
 	emailUsuario = document.getElementById("emailUsuario").value;
+	nombTarjeta = document.getElementById("Tnombre").value;
+	numTarjeta = document.getElementById("Tnumero").value;
+	mesTarjeta = document.getElementById("Tmes").value;
+	anioTarjeta = document.getElementById("Tanio").value;
+	cvvTarjeta = document.getElementById("Tcvv").value;
 	//verifica que no este vacios los campos
 	if (nombreUsuario !== "" && apellidoUsuario !== "" && fonoUsuario !== "" && emailUsuario !== "") {
 		//console.log("no estan vacios");
@@ -138,7 +197,12 @@ function actualizarDatosPerfilUsuario() {
 				Nombre: nombreUsuario,
 				Apellido: apellidoUsuario,
 				email: emailUsuario,
-				telefono: fonoUsuario
+				telefono: fonoUsuario,
+				nombTarjeta: nombTarjeta,
+				numTarjeta: numTarjeta,
+				mesTarjeta: mesTarjeta,
+				anioTarjeta: anioTarjeta,
+				cvvTarjeta: cvvTarjeta
 			})
 			.then(e => {
 				console.log("Datos Guardados Exitosamente datos generales");
--- js/Cliente/confUsuario.js
+++ js/Cliente/confUsuario.js
@@ -3,11 +3,11 @@ window.onload = ValidarCli();
 //Esta funcion habilita los inputs de configuracion de negocio
 function habilitarCamposPerfilUsuario() {
 	//console.log("here");
-	var auxNombre=document.getElementById("nombreUsuario").disabled;
-	var auxApellido=document.getElementById("apellidoUsuario").disabled;
-	var auxTelefono=document.getElementById("telefonoUsuario").disabled;
-	var auxEmail=document.getElementById("emailUsuario").disabled;
-	if  (auxNombre=== false && auxApellido===false && auxTelefono===false && auxEmail===false ) {
+	var auxNombre = document.getElementById("nombreUsuario").disabled;
+	var auxApellido = document.getElementById("apellidoUsuario").disabled;
+	var auxTelefono = document.getElementById("telefonoUsuario").disabled;
+	var auxEmail = document.getElementById("emailUsuario").disabled;
+	if (auxNombre === false && auxApellido === false && auxTelefono === false && auxEmail === false) {
 		bloquearCamposPefilUsuario();
 	} else {
 		document.getElementById("nombreUsuario").disabled = false;
@@ -17,40 +17,73 @@ function habilitarCamposPerfilUsuario() {
 	}
 	habilitarBtnGuardarCambios();
 }
+//Esta funcion habilita los inputs de configuracion para datos de tarjeta
+function habilitarCamposTarjeta() {
+	//console.log("here");
+	var auxnombre = document.getElementById("Tnombre").disabled;
+	var auxnumero = document.getElementById("Tnumero").disabled;
+	var auxmes = document.getElementById("Tmes").disabled;
+	var auxaño = document.getElementById("Tanio").disabled;
+	var auxcvv = document.getElementById("Tcvv").disabled;
+
+	if (
+		auxnombre === false &&
+		auxnumero === false &&
+		auxmes === false &&
+		auxaño === false &&
+		auxcvv === false
+	) {
+		bloquearCamposTarjeta();
+	} else {
+		document.getElementById("Tnombre").disabled = false;
+		document.getElementById("Tnumero").disabled = false;
+		document.getElementById("Tmes").disabled = false;
+		document.getElementById("Tanio").disabled = false;
+		document.getElementById("Tcvv").disabled = false;
+	}
+	habilitarBtnGuardarCambios();
+}
 //habilita los campos para cambios de perfil de usuario
 function habilitarBtnGuardarCambios() {
-	document.getElementById("btnGuardarCambios").disabled=false;
+	document.getElementById("btnGuardarCambios").disabled = false;
 }
 //bloquea los campos para cambios de perfil de usuario
-function bloquearCamposPefilUsuario(){
+function bloquearCamposPefilUsuario() {
 	document.getElementById("nombreUsuario").disabled = true;
 	document.getElementById("apellidoUsuario").disabled = true;
 	document.getElementById("telefonoUsuario").disabled = true;
 	document.getElementById("emailUsuario").disabled = true;
 }
+function bloquearCamposTarjeta() {
+	document.getElementById("Tnombre").disabled = true;
+	document.getElementById("Tnumero").disabled = true;
+	document.getElementById("Tmes").disabled = true;
+	document.getElementById("Tanio").disabled = true;
+	document.getElementById("Tcvv").disabled = true;
+}
 //guarda los cambios de perfil de usuario
 function GuardarCambiosPerfilUsuario() {
-	var btnGuardarCambios=document.getElementById("btnGuardarCambios").disabled;
-	if(btnGuardarCambios===false){
+	var btnGuardarCambios = document.getElementById("btnGuardarCambios").disabled;
+	if (btnGuardarCambios === false) {
 		console.log("click");
 		new Cliente().GuardarCambiosPerfilUsuario();
 		//new Negocio().GuardarCambiosAdministrador();
 		//document.getElementById("btnGuardarCambios").disabled=true;
-        bloquearCamposPefilUsuario();
+		bloquearCamposPefilUsuario();
 	}
 }
 //
-function ValidarCli(){
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-            // User is signed in.
-            var cli=new Cliente();
-            cli.setDatosPerfilUsuarioAct();
-            console.log("Logeado");
-        }else{
-            // User is not signed in.
-            console.log("No Logeado");
-            location.href="/html/index/usuarioIndex/indexUser.html"
-        }
-    });
+function ValidarCli() {
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+			// User is signed in.
+			var cli = new Cliente();
+			cli.setDatosPerfilUsuarioAct();
+			console.log("Logeado");
+		} else {
+			// User is not signed in.
+			console.log("No Logeado");
+			location.href = "/html/index/usuarioIndex/indexUser.html";
+		}
+	});
 }

d6fdf52a985cb95fa8df6ec3c61a9c46d88fe08a
JuanJo53 <juanjofer1999@gmail.com>
2019-12-18 17:47:09 -0400

    tabla de listas en clientes logrado

--- js/Cliente/Puestos.js
+++ js/Cliente/Puestos.js
@@ -2,18 +2,19 @@ window.onload = ValidarCli();
 //Coloca la imagen y nombre de los negocios disponibles.
 async function setNegocios() {
 	var arrayPuestos;
-	
+
 	var bd = firebase.firestore();
 	var string = "";
-	await bd.collection("Negocios")
+	await bd
+		.collection("Negocios")
 		.get()
 		.then(function(querySnapshot) {
 			querySnapshot.forEach(function(doc) {
 				string +=
-					`;<a href="./listasPuesto.html">
+					`;<a getNeg(this)>
 					<div class="pb-3 text-center puesto">
                         <div id="` +
-						doc.data().nombreNeg +
+					doc.data().nombreNeg +
 					`" onclick="getNeg(this)" class="card negocioIcon mx-auto">
                             <div class="img-puesto">
                                 <!--img del negocio-->
@@ -22,9 +23,9 @@ async function setNegocios() {
                             </div>
                             <div class="card-footer">
                                 <!--nombre del negocio-->
-                                <h5  class="my-auto text-light">` +
-								doc.data().nombreNeg +
-								`</h5>
+                                <h5 getNeg(this) id='negocio' class="my-auto text-light">` +
+					doc.data().nombreNeg +
+					`</h5>
                                 <!--//nombre del negocio-->
                             </div>
                         </div>
@@ -35,7 +36,7 @@ async function setNegocios() {
 				//
 				//console.log(cont);
 				//
-				
+
 				//if(cont==3){
 				//	cont=0;
 				//}
@@ -44,29 +45,29 @@ async function setNegocios() {
 		.catch(function(error) {
 			console.log("Error obteniendo los negocios: ", error);
 		});
-		var cont=0;
-		arrayPuestos=string;
-		var newArray = new Array();
-		newArray=arrayPuestos.split(";");
-		console.log("puestos "+newArray.length);
-		for(var i=0;i<newArray.length;i++){
-			if(newArray[i]!==""){
-				cont++;
-				var negocios = document.getElementById("Negocios"+cont);
-				//console.log(arrayPuestos[i]);
-				console.log(newArray[i]);
-				negocios.innerHTML = newArray[i];
-				if(cont==3){
-					cont=0;
-				}
+	var cont = 0;
+	arrayPuestos = string;
+	var newArray = new Array();
+	newArray = arrayPuestos.split(";");
+	console.log("puestos " + newArray.length);
+	for (var i = 0; i < newArray.length; i++) {
+		if (newArray[i] !== "") {
+			cont++;
+			var negocios = document.getElementById("Negocios" + cont);
+			//console.log(arrayPuestos[i]);
+			//console.log(newArray[i]);
+			negocios.innerHTML = newArray[i];
+			if (cont == 3) {
+				cont = 0;
 			}
-			
 		}
+	}
 }
 //Obtiene el Negocio elegido.
 function getNeg(objectPressed) {
 	negocioID = objectPressed.id;
 	console.log(negocioID);
+	window.location = "./listasPuesto.html?negocio=" + negocioID;
 }
 
 function ValidarCli() {

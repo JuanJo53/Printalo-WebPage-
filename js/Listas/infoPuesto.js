de98751c6cf8f4ac80c2962074611423e25e02ab
ariel <ariel.colque.herrera@gmail.com>
2019-12-18 20:57:10 -0400

    Se reparo lectura de horarios y seteo de datos

--- js/Listas/infoPuesto.js
+++ js/Listas/infoPuesto.js
@@ -13,6 +13,12 @@ async function getDocsData() {
 	var user = firebase.auth().currentUser;
 	var bd = firebase.firestore();
 	var dir, telef, email;
+	var entLunes,salLunes;
+	var entMartes,salMartes;
+	var entMiercoles,salMiercoles;
+	var entJueves,salJueves;
+	var entViernes,salViernes;
+	var entSabado,salSabado;
 	var negocioID;
 	await bd
 		.collection("Negocios")
@@ -24,16 +30,178 @@ async function getDocsData() {
 				dir = doc.data().dir;
 				telef = doc.data().fono;
 				email = doc.data().email;
+				entLunes=doc.data().horario.lunes.horaEntrada;
+				salLunes=doc.data().horario.lunes.horaSalida;
+				entMartes=doc.data().horario.martes.horaEntrada;
+				salMartes=doc.data().horario.martes.horaSalida;
+				entMiercoles=doc.data().horario.miercoles.horaEntrada;
+				salMiercoles=doc.data().horario.miercoles.horaSalida;
+				entJueves=doc.data().horario.jueves.horaEntrada;
+				salJueves=doc.data().horario.jueves.horaSalida;
+				entViernes=doc.data().horario.viernes.horaEntrada;
+				salViernes=doc.data().horario.viernes.horaSalida;
+				entSabado=doc.data().horario.sabado.horaEntrada;
+				salSabado=doc.data().horario.sabado.horaSalida;
 			});
 		});
 	console.log(negocioID);
-	setData(dir, telef, email);
+	setData(dir, telef, email,entLunes,salLunes,entMartes,salMartes,entMiercoles,salMiercoles,entJueves,salJueves,entViernes,salViernes,entSabado,salSabado);
 }
 
-function setData(dir, telef, email) {
+function setData(dir, telef, email,entLunes,salLunes,entMartes,salMartes,entMiercoles,salMiercoles,entJueves,salJueves,entViernes,salViernes,entSabado,salSabado) {
 	document.getElementById("direccion").innerHTML = `<span class="font-weight-bold">Calle : </span>` + dir;
 	document.getElementById("telefono").innerHTML = `<span class="font-weight-bold">Telefono : </span>` + telef;
 	document.getElementById("correo").innerHTML = `<span class="font-weight-bold">E-mail : </span>` + email;
+	document.getElementById("lunes").innerHTML = `<div class="input-group-prepend box">
+	<!--dia-->
+	<span class="input-group-text dia">Lunes</span>
+	<!--//dia-->
+	<!--horarios entreda-salida-->
+	<div class="input-group ">
+		<!--Input entrada-->
+		<input type="text" class="form-control text-center clock"
+			placeholder="`+entLunes+`" id="lunesEntrada" disabled>
+		<!--//Input entrada-->
+		<!--divisor-->
+		<div class="input-group-append ">
+			<!--Detalle - -->
+			<span
+				class="input-group-text bg-printalo-blueDetail bcero">-</span>
+			<!--Detalle - -->
+		</div>
+		<!--//divisor-->
+		<!--Input salida-->
+		<input type="text" class="form-control text-center clock"
+			placeholder="`+salLunes+`" id="lunesSalida" disabled>
+		<!--Input salida-->
+	</div>
+	<!--//horarios entreda-salida-->
+	</div>`;
+	document.getElementById("martes").innerHTML = `<div class="input-group-prepend box">
+	<!--dia-->
+	<span class="input-group-text dia">Martes</span>
+	<!--//dia-->
+	<!--horarios entreda-salida-->
+	<div class="input-group ">
+		<!--Input entrada-->
+		<input type="text" class="form-control text-center clock"
+			placeholder="`+entMartes+`" id="lunesEntrada" disabled>
+		<!--//Input entrada-->
+		<!--divisor-->
+		<div class="input-group-append ">
+			<!--Detalle - -->
+			<span
+				class="input-group-text bg-printalo-blueDetail bcero">-</span>
+			<!--Detalle - -->
+		</div>
+		<!--//divisor-->
+		<!--Input salida-->
+		<input type="text" class="form-control text-center clock"
+			placeholder="`+salMartes+`" id="lunesSalida" disabled>
+		<!--Input salida-->
+	</div>
+	<!--//horarios entreda-salida-->
+	</div>`;
+	document.getElementById("miercoles").innerHTML = `<div class="input-group-prepend box">
+	<!--dia-->
+	<span class="input-group-text dia">Miercoles</span>
+	<!--//dia-->
+	<!--horarios entreda-salida-->
+	<div class="input-group ">
+		<!--Input entrada-->
+		<input type="text" class="form-control text-center clock"
+			placeholder="`+entMiercoles+`" id="lunesEntrada" disabled>
+		<!--//Input entrada-->
+		<!--divisor-->
+		<div class="input-group-append ">
+			<!--Detalle - -->
+			<span
+				class="input-group-text bg-printalo-blueDetail bcero">-</span>
+			<!--Detalle - -->
+		</div>
+		<!--//divisor-->
+		<!--Input salida-->
+		<input type="text" class="form-control text-center clock"
+			placeholder="`+salMiercoles+`" id="lunesSalida" disabled>
+		<!--Input salida-->
+	</div>
+	<!--//horarios entreda-salida-->
+	</div>`;
+	document.getElementById("jueves").innerHTML = `<div class="input-group-prepend box">
+	<!--dia-->
+	<span class="input-group-text dia">Jueves</span>
+	<!--//dia-->
+	<!--horarios entreda-salida-->
+	<div class="input-group ">
+		<!--Input entrada-->
+		<input type="text" class="form-control text-center clock"
+			placeholder="`+entJueves+`" id="lunesEntrada" disabled>
+		<!--//Input entrada-->
+		<!--divisor-->
+		<div class="input-group-append ">
+			<!--Detalle - -->
+			<span
+				class="input-group-text bg-printalo-blueDetail bcero">-</span>
+			<!--Detalle - -->
+		</div>
+		<!--//divisor-->
+		<!--Input salida-->
+		<input type="text" class="form-control text-center clock"
+			placeholder="`+salJueves+`" id="lunesSalida" disabled>
+		<!--Input salida-->
+	</div>
+	<!--//horarios entreda-salida-->
+	</div>`;
+	document.getElementById("viernes").innerHTML = `<div class="input-group-prepend box">
+	<!--dia-->
+	<span class="input-group-text dia">Viernes</span>
+	<!--//dia-->
+	<!--horarios entreda-salida-->
+	<div class="input-group ">
+		<!--Input entrada-->
+		<input type="text" class="form-control text-center clock"
+			placeholder="`+entViernes+`" id="lunesEntrada" disabled>
+		<!--//Input entrada-->
+		<!--divisor-->
+		<div class="input-group-append ">
+			<!--Detalle - -->
+			<span
+				class="input-group-text bg-printalo-blueDetail bcero">-</span>
+			<!--Detalle - -->
+		</div>
+		<!--//divisor-->
+		<!--Input salida-->
+		<input type="text" class="form-control text-center clock"
+			placeholder="`+salViernes+`" id="lunesSalida" disabled>
+		<!--Input salida-->
+	</div>
+	<!--//horarios entreda-salida-->
+	</div>`;
+	document.getElementById("sabado").innerHTML = `<div class="input-group-prepend box">
+	<!--dia-->
+	<span class="input-group-text dia">Sabado</span>
+	<!--//dia-->
+	<!--horarios entreda-salida-->
+	<div class="input-group ">
+		<!--Input entrada-->
+		<input type="text" class="form-control text-center clock"
+			placeholder="`+entSabado+`" id="lunesEntrada" disabled>
+		<!--//Input entrada-->
+		<!--divisor-->
+		<div class="input-group-append ">
+			<!--Detalle - -->
+			<span
+				class="input-group-text bg-printalo-blueDetail bcero">-</span>
+			<!--Detalle - -->
+		</div>
+		<!--//divisor-->
+		<!--Input salida-->
+		<input type="text" class="form-control text-center clock"
+			placeholder="`+salSabado+`" id="lunesSalida" disabled>
+		<!--Input salida-->
+	</div>
+	<!--//horarios entreda-salida-->
+	</div>`;
 }
 var nombre;
 function getNeg() {
